57a1c9d8239365d8348bbbb0495b8ecd87715aa3
yanki levine <70327894+yaakovlewin@users.noreply.github.com>
2023-12-10 16:00:45 +0000

    enhances the testimonials

--- components/TestimonialCard.jsx
+++ components/TestimonialCard.jsx
@@ -1,19 +1,25 @@
 import Image from "next/image";
 
-export default function TestimonialCard({ quote, author, imageSrc }) {
+export default function TestimonialCard({ testimonial, isHighlighted }) {
     return (
-        <div className="flex flex-col items-center bg-white p-6 shadow rounded-lg">
+        <div
+            className={`flex flex-col items-center bg-white p-4 shadow rounded-lg transition-transform duration-300 ${
+                isHighlighted
+                    ? "opacity-100t ransform scale-110"
+                    : "opacity-50 transform scale-90"
+            }`}
+        >
             <Image
-                src={imageSrc}
-                alt={author}
+                src={testimonial.imageSrc}
+                alt={testimonial.author}
                 width={80}
                 height={80}
                 className="w-16 h-16 rounded-full mb-3"
             />
             <blockquote className="text-gray-600 italic mb-4">
-                "{quote}"
+                "{testimonial.quote}"
             </blockquote>
-            <p className="font-semibold">{author}</p>
+            <p className="font-semibold">{testimonial.author}</p>
         </div>
     );
 }
--- components/TestimonialsSection.jsx
+++ components/TestimonialsSection.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import TestimonialCard from "./TestimonialCard";
 
 const testimonials = [
@@ -17,33 +18,63 @@ const testimonials = [
         author: "Emily Johnson",
         imageSrc: "/images/testimonial-3.jpg", // Replace with actual image path
     },
+    {
+        quote: "MortgageCo provided outstanding service and helped me secure the best rate for my home loan.",
+        author: "John Doe",
+        imageSrc: "/images/testimonial-1.jpg", // Replace with actual image path
+    },
     // ... add more testimonials as needed ...
 ];
 
 export default function TestimonialsSection() {
+    const [activeIndex, setActiveIndex] = useState(1);
+
+    useEffect(() => {
+        // Adjust this timeout value as needed for automatic transition
+        const interval = setInterval(() => {
+            setActiveIndex(
+                (prevIndex) => (prevIndex + 1) % testimonials.length
+            );
+        }, 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+    const visibleTestimonials = [
+        testimonials[
+            (activeIndex + testimonials.length - 1) % testimonials.length
+        ],
+        testimonials[activeIndex],
+        testimonials[(activeIndex + 1) % testimonials.length],
+    ];
+
     return (
         <section className="bg-gray-100 py-12">
             <div className="container mx-auto px-4">
                 <h2 className="text-2xl font-bold text-center mb-6">
                     What Our Clients Say
                 </h2>
-                <div className="grid md:grid-cols-3 gap-6">
-                    <TestimonialCard
-                        quote="MortgageCo provided outstanding service and helped me secure the best rate for my home loan."
-                        author="John Doe"
-                        imageSrc="/images/testimonial-1.jpg" // Replace with actual image path
-                    />
-                    <TestimonialCard
-                        quote="The team at MortgageCo was incredibly helpful and made the mortgage process seamless and stress-free."
-                        author="Jane Smith"
-                        imageSrc="/images/testimonial-2.jpg" // Replace with actual image path
-                    />
-                    <TestimonialCard
-                        quote="I highly recommend MortgageCo for their professionalism and expertise in the mortgage industry."
-                        author="Emily Johnson"
-                        imageSrc="/images/testimonial-3.jpg" // Replace with actual image path
-                    />
-                    {/* Add more TestimonialCards as needed */}
+                <div className="flex justify-center gap-4">
+                    {visibleTestimonials.map((testimonial, index) => (
+                        <TestimonialCard
+                            key={index}
+                            testimonial={testimonial}
+                            isHighlighted={index === 1}
+                        />
+                    ))}
+                </div>
+
+                <div className="flex justify-center mt-4">
+                    {testimonials.map((_, index) => (
+                        <span
+                            key={index}
+                            onClick={() => setActiveIndex(index)}
+                            className={`inline-block h-3 w-3 mx-1 rounded-full cursor-pointer ${
+                                index === activeIndex
+                                    ? "bg-green-500"
+                                    : "bg-gray-400"
+                            }`}
+                        ></span>
+                    ))}
                 </div>
             </div>
         </section>
