40f5c7b513504edf65bd084379759bf9f3ecfc70
yanki levine <70327894+yaakovlewin@users.noreply.github.com>
2024-02-19 12:42:00 +0000

    Refactor ServiceHeroSection styling

--- components/services/ServiceHeroSection.jsx
+++ components/services/ServiceHeroSection.jsx
@@ -2,25 +2,27 @@ import Link from "next/link";
 
 export default function ServiceHeroSection({ service }) {
   return (
-    <div
-      className="flex min-h-[40vh] items-center bg-cyan-50 bg-opacity-70 bg-cover bg-center object-fill px-10 py-24 text-white text-shadow-black-border-sm sm:h-[50vh] md:h-[60vh] lg:h-[70vh]"
-      style={{ backgroundImage: `url(${service.imageUrl})` }}
-    >
-      {" "}
-      <div className="rounded-lg bg-teal-50 bg-opacity-70 p-4 py-7 text-shadow-black md:w-1/3">
-        <h1 className="mb-4 text-4xl font-bold text-shadow-black-lg">
-          {service.title}
-        </h1>
-        <p className="mb-6 w-fit bg-white bg-opacity-5 text-xl text-sky-950 text-shadow-white">
-          {service.description}
-        </p>
-        <Link
-          href="#learn-more"
-          className="rounded bg-cyan-500 px-6 py-3 font-bold uppercase text-white text-shadow-none hover:bg-cyan-600"
-        >
-          Get Started
-        </Link>
-      </div>
+    <div className="bg-sky-300 heropattern-tinycheckers-white">
+      <section
+        className="mx-auto flex min-h-[40vh] max-w-[2000px] items-center bg-cyan-50 bg-opacity-70 bg-cover bg-center object-fill px-10 py-24 text-white text-shadow-black-border-sm sm:h-[50vh] md:h-[60vh] lg:h-[70vh] "
+        style={{ backgroundImage: `url(${service.imageUrl})` }}
+      >
+        {" "}
+        <article className="rounded-lg bg-teal-50 bg-opacity-70 p-4 py-7 text-shadow-black md:w-1/3">
+          <h1 className="mb-4 text-4xl font-bold text-shadow-black-lg">
+            {service.title}
+          </h1>
+          <p className="mb-6 w-fit bg-white bg-opacity-5 text-xl text-sky-950 text-shadow-white">
+            {service.description}
+          </p>
+          <Link
+            href="#learn-more"
+            className="rounded bg-cyan-500 px-6 py-3 font-bold uppercase text-white text-shadow-none hover:bg-cyan-600"
+          >
+            Get Started
+          </Link>
+        </article>
+      </section>
     </div>
   );
 }
