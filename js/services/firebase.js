731790a9dba32f83dcd23869acc9f08ea83e61bc
yanki levine <70327894+yaakovlewin@users.noreply.github.com>
2024-09-17 23:49:45 +0100

    refactor(firebase): update initializeAppCheck to include app parameter and streamline app check initialization

--- js/services/firebase.js
+++ js/services/firebase.js
@@ -1,17 +1,36 @@
 import { firebaseConfig } from "@/js/config/firebaseConfig";
 import { initializeApp } from "firebase/app";
-import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 import { getFirestore } from "firebase/firestore";
 
-const SiteKey = process.env.reCAPTCHA_site_key;
-
+// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
 
-const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider(SiteKey),
-  isTokenAutoRefreshEnabled: true,
-});
+// Check if window is undefined (i.e., if running on the server)
+if (typeof window !== "undefined") {
+  // Only import and initialize App Check if we are in the browser
 
+  import("firebase/app-check")
+    .then(({ initializeAppCheck, ReCaptchaV3Provider }) => {
+      const SiteKey = process.env.reCAPTCHA_site_key;
+
+      if (!SiteKey) {
+        console.error(
+          "reCAPTCHA site key is not defined. Please check your environment variables.",
+        );
+      }
+
+      // Initialize App Check with reCAPTCHA V3
+      initializeAppCheck(app, {
+        provider: new ReCaptchaV3Provider(SiteKey),
+        isTokenAutoRefreshEnabled: true,
+      });
+    })
+    .catch((error) => {
+      console.error("Error loading Firebase App Check:", error);
+    });
+}
+
+// Initialize Firestore Database
 const db = getFirestore(app);
 
 export { app, db };
