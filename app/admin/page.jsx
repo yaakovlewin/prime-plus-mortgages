081b079aa544ef8e198d203cafc110cb73591897
yanki levine <70327894+yaakovlewin@users.noreply.github.com>
2024-09-17 18:59:38 +0100

    refactor(admin): remove unused authentication code and streamline application fetching feat(firebase): add App Check initialization with reCAPTCHA support

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -26,52 +26,7 @@ const AdminPage = () => {
     }
     fetchApplications();
   }, []);
-  //   const [user, setUser] = useState(null);
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [applications, setApplications] = useState([]);
-  // const [error, setError] = useState(null);
 
-  //   const auth = getAuth(app);
-
-  //   useEffect(() => {
-  //     const unsubscribe = auth.onAuthStateChanged((user) => {
-  //       setUser(user);
-  //       if (user) {
-  //         fetchApplications();
-  //       }
-  //     });
-
-  //     return () => unsubscribe();
-  //   }, [auth]);
-
-  //   const handleLogin = async (e) => {
-  //     e.preventDefault();
-  //     try {
-  //       await signInWithEmailAndPassword(auth, email, password);
-  //     } catch (error) {
-  //       setError(error.message);
-  //     }
-  //   };
-
-  //   const handleLogout = async () => {
-  //     try {
-  //       await signOut(auth);
-  //     } catch (error) {
-  //       setError(error.message);
-  //     }
-  //   };
-
-  // const fetchApplications = async () => {
-  //   try {
-  //     const applications = await getCollection("applicationForms");
-  //     setApplications(applications);
-  //   } catch (error) {
-  //     setError("Error fetching applications: " + error.message);
-  //   }
-  // };
-
-  // fetchApplications();
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
@@ -106,39 +61,7 @@ const AdminPage = () => {
           </tbody>
         </table>
       </div>
-      {/* <Heading2>Admin Dashboard</Heading2> */}
-      {/* {!user ? (
-        <form onSubmit={handleLogin} className="mb-4">
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-            className="mb-2 w-full rounded border p-2"
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            className="mb-2 w-full rounded border p-2"
-          />
-          <button
-            type="submit"
-            className="rounded bg-cyan-500 px-4 py-2 text-white"
-          >
-            Login
-          </button>
-        </form>
-      ) : (
-        <div>
-          <p className="mb-4">Welcome, {user.email}</p>
-          <button
-            onClick={handleLogout}
-            className="mb-4 rounded bg-red-500 px-4 py-2 text-white"
-          >
-            Logout
-          </button> */}
+
       <h3 className="mb-2 text-xl font-bold">Application Forms</h3>
       {applications.map((app, index) => (
         <div key={index} className="mb-4 rounded border p-4">
--- js/services/firebase.js
+++ js/services/firebase.js
@@ -1,8 +1,17 @@
 import { firebaseConfig } from "@/js/config/firebaseConfig";
 import { initializeApp } from "firebase/app";
+import { initiolizeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 import { getFirestore } from "firebase/firestore";
 
+const SiteKey = process.env.reCAPTCHA_site_key;
+
 const app = initializeApp(firebaseConfig);
+
+const appCheck = initiolizeAppCheck({
+  provider: new ReCaptchaV3Provider(SiteKey),
+  isTokenAutoRefreshEnabled: true,
+});
+
 const db = getFirestore(app);
 
 export { app, db };
