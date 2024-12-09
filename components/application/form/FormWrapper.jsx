17a61b08010ea79a6d3caf06e7ef1686d655c418
yanki levine <70327894+yaakovlewin@users.noreply.github.com>
2024-12-09 16:23:39 +0000

    feat(form): introduce FormWrapper component for enhanced form management
    Added a new FormWrapper component to encapsulate the use of CustomFormProvider and RHFFormProvider. This simplifies form management by managing context and validation logic in one place. Modified RenderForm to utilize this wrapper, integrating react-hook-form with zod schema validation for improved form handling and error management. This change promotes cleaner code and better scalability for forms within the application.

---
+++ components/application/form/FormWrapper.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { FormProvider as CustomFormProvider } from "components/application/FormContext";
+import { FormProvider as RHFFormProvider } from "react-hook-form";
+
+export default function FormWrapper({ children, methods, formSteps }) {
+  return (
+    <CustomFormProvider initialSteps={formSteps}>
+      <RHFFormProvider {...methods}>{children}</RHFFormProvider>
+    </CustomFormProvider>
+  );
+}
--- components/application/form/RenderForm.jsx
+++ components/application/form/RenderForm.jsx
@@ -1,15 +1,28 @@
 "use client";
-import { FormProvider } from "@/components/application/FormContext";
-import FormContentRenderer from "@/components/application/form/FormContentRenderer";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import FormContentRenderer from "components/application/form/FormContentRenderer";
+import applicationSchema from "js/zod/mortgageValidationSchema";
+import { useForm } from "react-hook-form";
+import FormWrapper from "./FormWrapper";
 
 export default function RenderForm({ config }) {
   const formSteps = config.steps;
 
+  const methods = useForm({
+    mode: "onBlur",
+    resolver: zodResolver(applicationSchema),
+    defaultValues: {
+      formType: config.formType,
+      applicants: [{}],
+    },
+  });
+
   return (
     <div className="">
-      <FormProvider initialSteps={formSteps}>
+      <FormWrapper methods={methods} formSteps={formSteps}>
         <FormContentRenderer config={config} />
-      </FormProvider>
+      </FormWrapper>
     </div>
   );
 }
